fed1de8e8ec00a499fab7fcfb5bfe9f03f92daa3
Yair Fernando <sistemasalbardas.2@gmail.com>
2018-07-23 10:24:45 -0500

    commit

--- VIEWS/template.php
+++ VIEWS/template.php
@@ -27,14 +27,15 @@
     <link rel="stylesheet" href="http://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
 	<!-- nuevos link -->
-	  <link rel="stylesheet" href="<?= URL?>/VIEWS/bower_components/bootstrap/dist/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="<?= URL?>/VIEWS/resources/bower_components/bootstrap/dist/css/bootstrap.min.css">
 	  <!-- Font Awesome -->
 	  <link rel="stylesheet" href="<?= URL?>/VIEWS/resources/CSS/all.min.css">
 	  <!-- Ionicons -->
-	  <link rel="stylesheet" href="<?= URL?>/VIEWS/bower_components/Ionicons/css/ionicons.min.css">
+	  <link rel="stylesheet" href="<?= URL?>/VIEWS/resources/bower_components/Ionicons/css/ionicons.min.css">
 	  <!-- jvectormap -->
-	  <link rel="stylesheet" href="<?= URL?>/VIEWS/bower_components/jvectormap/jquery-jvectormap.css">
+	  <link rel="stylesheet" href="<?= URL?>/VIEWS/resources/bower_components/jvectormap/jquery-jvectormap.css">
 	  <!-- Theme style -->
+	  <link rel="stylesheet" href="<?= URL?>/VIEWS/resources/bower_components/bootstrap-select/css/boostrap-select.min.css">
 	  <link rel="stylesheet" href="<?= URL?>/VIEWS/dist/css/AdminLTE.min.css">
 	  <link rel="stylesheet" href="<?= URL?>/VIEWS/resources/CSS/animate.css">
 	  <link rel="stylesheet" href="<?= URL?>/VIEWS/dist/css/skins/_all-skins.min.css">
@@ -98,6 +99,7 @@
 	<script src="<?= URL?>/VIEWS/resources/JS/bootstrap-select.js"></script>
 	<script src="<?= URL?>/VIEWS/resources/JS/ajax.js"></script>
 	<script src="<?= URL?>/VIEWS/resources/JS/jquery-ui/jquery-ui.min.js"></script>
+	<script src="<?= URL?>/VIEWS/resources/bower_components/bootstrap-select/js/bootstrap-select.min.js"></script>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 	<script src="http://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 	<script src="http://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"></script>
